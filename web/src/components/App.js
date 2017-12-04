a8febfb5842c351bf87970b09e76afb246768f64
Renington Neri <renington.neri@gmail.com>
2017-12-04 20:42:46 -0200

    refac: Converting stateful to stateless functional component

--- web/src/components/App.js
+++ web/src/components/App.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Home from './Home'
 import PostsFiltered from './PostsFiltered'
 import PostPage from './PostPage'
 import NotFound from './NotFound'
 
-class App extends Component {
-    render() {
-        return (
-            <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/:category" component={PostsFiltered} />
-                <Route exact path='/:category/:postId' component={PostPage} />
-                <Route component={NotFound} />
-            </Switch>
-        );
-    }
+const App = () => {
+    return (
+        <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/:category" component={PostsFiltered} />
+            <Route exact path='/:category/:postId' component={PostPage} />
+            <Route component={NotFound} />
+        </Switch>
+    )
 }
 
-export default App;
+export default App
