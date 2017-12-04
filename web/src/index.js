2a56bdb2baf214a2553ea3c910b2ac34c0a3cf6b
Renington Neri <renington.neri@gmail.com>
2017-12-04 20:51:08 -0200

    refac: adding redux-logger lib

--- web/src/index.js
+++ web/src/index.js
@@ -8,6 +8,7 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import reducer from './reducers/index';
 import { Provider } from "react-redux";
 
+import logger from 'redux-logger'
 import Thunk from 'redux-thunk';
 import { BrowserRouter as Router } from 'react-router-dom'
 
@@ -16,7 +17,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const store = createStore(
     reducer, 
     {},
-    composeEnhancers(applyMiddleware(Thunk))
+    composeEnhancers(applyMiddleware(Thunk, logger))
 );
 
 ReactDOM.render(
